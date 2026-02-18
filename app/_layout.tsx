035a835142366e84117111954cb791304e006c3a
Jeanne Grenet <jeanne.grenet@gmail.com>
2026-02-18 11:25:10 +0100

    feat: setup clerk and auth

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,15 @@
-import { Slot } from "expo-router";
+import { Stack } from "expo-router";
+import { tokenCache } from "@clerk/clerk-expo/token-cache";
+import { ClerkProvider } from "@clerk/clerk-expo";
+
 const RootLayout = () => {
-  return <Slot />;
+  return (
+    <ClerkProvider tokenCache={tokenCache}>
+      <Stack>
+        <Stack.Screen name="(home)" options={{ headerShown: false }} />
+        <Stack.Screen name="(auth)" options={{ headerShown: false }} />
+      </Stack>
+    </ClerkProvider>
+  );
 };
 export default RootLayout;
